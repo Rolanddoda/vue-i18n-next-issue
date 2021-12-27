d9437458442329f7e9c813b2344c6d58e8fb6292
Roland Doda <rolanddoda2014@gmail.com>
2021-12-27 17:08:55 +0100

    Add "Local scope based localization"

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page {{ $t("page") }}</h1>
+    <p>{{ $t("localMsg") }}</p>
   </div>
 </template>
+
+<script>
+import { useI18n } from "vue-i18n";
+
+export default {
+  setup() {
+    const { t } = useI18n({
+      locale: "en",
+      messages: {
+        en: {
+          localMsg: "This is a local message",
+        },
+      },
+    });
+
+    return { t };
+  },
+};
+</script>
